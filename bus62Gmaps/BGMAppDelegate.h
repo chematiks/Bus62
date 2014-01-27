7ff10ce140a6dd75beb1ffaab2bebcbcbd98a532
Admin <admin@Admins-Mac.local>
2014-01-27 15:39:58 +0400

    Initial Commit

---
+++ bus62Gmaps/BGMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BGMAppDelegate.h
+//  bus62Gmaps
+//
+//  Created by Admin on 27.01.14.
+//  Copyright (c) 2014 Chematiks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bus62Gmaps/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bus62Gmaps
+//
+//  Created by Admin on 27.01.14.
+//  Copyright (c) 2014 Chematiks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGMAppDelegate class]));
+    }
+}
